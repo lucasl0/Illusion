775b82db43a8ac256ea7754ebd1e429230a74bd6
lucasl0 <lucas2005leo@gmail.com>
2025-01-30 09:23:32 +0100

    klynt haha

--- miniPlayerData.js
+++ miniPlayerData.js
@@ -1,1 +1,15 @@
-klynt.miniPlayerData = {"downloadAppWording":"Télécharger l'app","thanksForWatchingWording":"Merci de votre visite !","launchAppWording":"Et lancer le projet","resumePlaybackWording":"Relancer la lecture ?","fullscreenInfoWording":"Ce programme va se lancer en plein écran","yesWording":"Oui","title":"Illusion","thumbnail":"Medias/Thumbnails/Affiche_n_3_-_Illusion.jpg.jpg","noWording":"Non","analyticsKey":"","redirectToMobileApp":"auto","url":"","description":"Fiction-interactive-policière"}
+klynt.miniPlayerData = {
+    "thanksForWatchingWording": "Merci de votre visite !",
+    "downloadAppWording": "Télécharger l'app",
+    "fullscreenInfoWording": "Ce programme va se lancer en plein écran",
+    "yesWording": "Oui",
+    "title": "Illusion",
+    "launchAppWording": "Et lancer le projet",
+    "noWording": "Non",
+    "analyticsKey": "",
+    "redirectToMobileApp": "auto",
+    "resumePlaybackWording": "Relancer la lecture ?",
+    "url": "",
+    "thumbnail": "Medias/Thumbnails/Affiche_n_3_-_Illusion.jpg.jpg",
+    "description": "Fiction-interactive-policière"
+}
